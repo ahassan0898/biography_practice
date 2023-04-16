15369e5d69139733a8551bb66a955204b9cc0f7b
Ayat Hassan <a.hassan0898@gmail.com>
2023-04-16 10:13:32 -0500

    Author

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Author {
 
     /**
@@ -8,18 +10,47 @@ public class Author {
 
     //Create 6 args custom constructor here
     //YOUR CODE HERE
+    public Author (String firstName, String lastName, String country,
+                   boolean isAlive){
+        this.firstName=firstName;
+        this.lastName= lastName;
+        this.country = country;
+        this.isAlive=isAlive;
+
+    }
+    public Author (String firstName, String lastName, String country,
+                   boolean isAlive, int age, ArrayList<Book> listOfBooks){
+        this.firstName=firstName;
+        this.lastName= lastName;
+        this.country = country;
+        this.isAlive=isAlive;
+        this.age=age;
+        this.listOfBooks=listOfBooks;
+
+    }
 
 
     /*
         Define instance variables here
         NOTE: Books must be defined as a List of Objects of Book class not String
     */
-    //YOUR CODE HERE
+    public String firstName,lastName,country;
+    public int age;
+    public boolean isAlive;
+    public ArrayList<Book> listOfBooks;
 
 
     /*
     Override toString() method here that returns Author object information
      */
-    //YOUR CODE HERE
 
+    @Override
+    public String toString() {
+        return "Author{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", country='" + country + '\'' +
+                ", isAlive=" + isAlive +
+                '}';
+    }
 }
--- src/Book.java
+++ src/Book.java
@@ -2,22 +2,36 @@ public class Book {
 
     /**
      * COMPLETE THIS CLASS FIRST, THEN CONTINUE WITH AUTHOR AND BIOGRAPHY
-    Create a custom constructor that will take 3 args and defines the Book object
-    Define Book class fields as name, genre, totalPage
+     * Create a custom constructor that will take 3 args and defines the Book object
+     * Define Book class fields as name, genre, totalPage
      */
 
     //Create 3 args custom constructor here
-    //YOUR CODE HERE
 
+    public Book(String name, String genre, int totalPage) { //name of class
+        this.name = name;
+        this.genre = genre;
+        this.totalPage = totalPage;
+    }
 
-    //Define instance variables here
-    //YOUR CODE HERE
+    //Define instance variables here // features that define an oject
 
+    public String name, genre;
+    public int totalPage;
 
 
     /*
         Override toString() method that returns Book object information
      */
-    //YOUR CODE HERE
+    //generate-toString(), click on all
 
+    @Override
+    public String toString() { // if you delete override method, you will get location because toString is not there anymore
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", genre='" + genre + '\'' +
+                ", totalPage=" + totalPage +
+                '}';
+    }
 }
+
